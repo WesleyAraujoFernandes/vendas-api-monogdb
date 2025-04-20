a3e126c318c3a88a2c6d05606b777e6f8681f664
gubertevandro001 <gubertevandro33@gmail.com>
2025-04-20 19:35:39 -0300

    refactor: mudança na descrição do swagger

--- src/main/java/br/com/vendas/api/v1/openapi/VendaOpenAPI.java
+++ src/main/java/br/com/vendas/api/v1/openapi/VendaOpenAPI.java
@@ -3,13 +3,16 @@ package br.com.vendas.api.v1.openapi;
 import br.com.vendas.api.v1.request.CadastrarVendaRequest;
 import br.com.vendas.api.v1.request.ItemVendaRequest;
 import br.com.vendas.api.v1.response.CadastrarVendaResponse;
+import br.com.vendas.domain.Pagination;
+import br.com.vendas.domain.filter.VendaFilter;
+import br.com.vendas.domain.model.Venda;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Vendas", description = "Gerencia as vendas")
 public interface VendaOpenAPI {
@@ -34,4 +37,18 @@ public interface VendaOpenAPI {
     @ApiResponse(responseCode = "400", description = "Erro de validação")
     @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
     ResponseEntity<Void> removerItemVenda(@PathVariable("vendaId") String vendaId, @RequestBody ItemVendaRequest request);
+
+    @GetMapping
+    @Operation(summary = "Lista as vendas com base nos parâmetros informados")
+    @Parameter(name = "clienteId", description = "ID do cliente")
+    @Parameter(name = "dataInicio", description = "Data de início da venda")
+    @Parameter(name = "dataFim", description = "Data de fim da venda")
+    @Parameter(name = "pagina", description = "Número da página")
+    @Parameter(name = "porPagina", description = "Número de itens por página")
+    @Parameter(name = "ordenarPor", description = "Campo para ordenação")
+    @Parameter(name = "ordem", description = "Direção da ordenação (asc ou desc)")
+    @ApiResponse(responseCode = "200", description = "Vendas encontradas com sucesso")
+    @ApiResponse(responseCode = "400", description = "Erro de validação")
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    ResponseEntity<Pagination<Venda>> buscarVendas(@ModelAttribute VendaFilter vendaFilter);
 }
