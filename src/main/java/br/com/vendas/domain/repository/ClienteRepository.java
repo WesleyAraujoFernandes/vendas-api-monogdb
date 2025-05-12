f0046e58273b0507cc82fdc2980dddd3dc638042
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-05-12 14:56:30 -0300

    Alteração no endereço

--- src/main/java/br/com/vendas/domain/repository/ClienteRepository.java
+++ src/main/java/br/com/vendas/domain/repository/ClienteRepository.java
@@ -2,8 +2,6 @@ package br.com.vendas.domain.repository;
 
 import br.com.vendas.domain.model.Cliente;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
 import java.util.List;
 
 public interface ClienteRepository extends MongoRepository<Cliente, String> {
